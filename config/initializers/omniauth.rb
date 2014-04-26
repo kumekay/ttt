116aa87ac94bf8e736c9f259b8694c2e4c49e547
Markov Alexey <piton4eg@mail.ru>
2014-04-26 14:40:09 +0400

    OK auth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
+  provider :odnoklassniki, ENV['OK_ID'], ENV['OK_SECRET_KEY'], :public_key => ENV['OK_PUBLIC_KEY']
 end
