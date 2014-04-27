9a0c23eb900a55ea8696a0c798bd866dd1366bd2
Markov Alexey <piton4eg@mail.ru>
2014-04-27 06:26:53 +0400

    Fix

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,8 +2,9 @@ class LikesController < ApplicationController
   def like_toggle
     @likes = current_user.likes.where(stuff_id: params[:stuff_id])
     if like = @likes.first
-      bucket = Bucket.find_by(user: current_user, stuff_id: params[:stuff_id])
-      bucket.destroy
+      if bucket = Bucket.find_by(user: current_user, stuff_id: params[:stuff_id])
+        bucket.destroy
+      end
       like.destroy
       render :unlike
     else
