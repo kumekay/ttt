e3b2bd41f9359625d92a9aa10ec25cd74abdb1ef
Markov Alexey <piton4eg@mail.ru>
2014-04-27 17:12:52 +0400

    Last steps

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_filter :authorize
+  before_filter :check_cookie
 
   helper_method :current_user
 
@@ -11,6 +12,13 @@ class ApplicationController < ActionController::Base
   def authorize
     unless User.where(id: session[:user_id]).first
       redirect_to root_url
+    end
+  end
+
+  def check_cookie
+    unless cookies[:showed_welcome_page]
+      cookies[:showed_welcome_page] = { value: true }
+      redirect_to welcome_path
     end
   end
 
